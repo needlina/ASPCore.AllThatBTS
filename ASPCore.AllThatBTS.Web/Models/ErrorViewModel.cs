61568d7e696b2eefeaef61c47a60865fbdfdd59f
LeeHyunjin <needlina@hotmail.com>
2018-11-03 09:56:41 +0900

    Initial Commit

---
+++ ASPCore.AllThatBTS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPCore.AllThatBTS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
